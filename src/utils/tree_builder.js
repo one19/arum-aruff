6d15efb32b5ceec66f701b15e98bec19227fc880
Drew Showalter <maelstroswim@gmail.com>
2018-07-29 15:04:59 +1000

    :sparkles: add a tree builder util.

--- src/utils/tree_builder.js
+++ src/utils/tree_builder.js
@@ -1,1 +1,45 @@
-export default () => 'setup is working';
+import { promises as fs } from 'fs';
+import { default as GitIgnorer } from './gitignorer';
+
+const buildTree = path => {
+  const ignorer = new GitIgnorer(path);
+
+  return traverseTree(path, ignorer.test);
+};
+
+/**
+ * Function fires off a nasty load of promises and tries
+ * to generate a bare and easily traversible tree object
+ * of the filesystem, at the location, defaulting to ./
+ *
+ * @async
+ * @function traverseTree
+ * @param {string} [path=.] - root directory
+ * @returns {Promise} promise that resolves to a dir tree or breaks
+ */
+const traverseTree = async (path = '.', ignorer = () => {}) => {
+  const stats = await fs.stat(`${path}/`);
+
+  if (stats.isDirectory()) {
+    const subdirs = await fs.readdir(`${path}/`);
+
+    const filteredSubdirs = subdirs.filter(ignorer);
+
+    const subBranches = await Promise.all(
+      filteredSubdirs.map(dirname =>
+        traverseTree(`${path}/${dirname}`, ignorer)
+      )
+    );
+
+    const subTree = filteredSubdirs.reduce(
+      (thisTree, dirname, i) => ({ ...thisTree, [dirname]: subBranches[i] }),
+      {}
+    );
+
+    return { ...subTree, dir: true };
+  }
+
+  return {};
+};
+
+export default buildTree;
--- test/utils/tree_builder_test.js
+++ test/utils/tree_builder_test.js
@@ -1,6 +1,61 @@
 import test from 'ava';
+import mock from 'mock-fs';
 import { buildTree } from '../../src/utils';
 
-test('builds a single-level tree for the current dir', t => {
-  t.deepEqual(buildTree(), 'setup is working');
+test.afterEach.always(() => mock.restore());
+
+test.serial('creates object tree of a directory', async t => {
+  mock({
+    folder_bingo: {
+      '.gitignore': 'this must be here to keep mock-fs from dying',
+      'index.js': "import stuff from './src/doSomething';",
+      src: {
+        'doSomething.js': 'let basically_nothing = "doing";'
+      }
+    }
+  });
+
+  const tree = await buildTree('./folder_bingo');
+
+  t.deepEqual(tree, {
+    src: { dir: true, 'doSomething.js': {} },
+    'index.js': {},
+    dir: true
+  });
+});
+
+test.serial('defaults to ./ when not given a dir', async t => {
+  mock({
+    '.gitignore': 'primble',
+    'index.js': "import stuff from './src/doSomething';"
+  });
+
+  const tree = await buildTree();
+
+  t.deepEqual(tree, {
+    'index.js': {},
+    dir: true
+  });
+});
+
+test.serial('ignores . & gitignored folders/files defined in ./', async t => {
+  mock({
+    'index.js': "import stuff from './src/doSomething';",
+    '.gitignore': `
+      node_modules
+      *.map
+    `,
+    node_modules: {
+      lots: { of: { junk: 'like wow' } }
+    },
+    fleeb: { plumbus: { 'dingle.map': 'c[r]a[z]y' } }
+  });
+
+  const tree = await buildTree();
+
+  t.deepEqual(tree, {
+    fleeb: { plumbus: { dir: true }, dir: true },
+    'index.js': {},
+    dir: true
+  });
 });
