80bfe0f6cd82c0d1383ad0866c746500528c37a6
Drew Showalter <maelstroswim@gmail.com>
2018-08-11 20:17:27 +1000

    :sparkles: add simplest execution chain.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('do a fuckin thing ey');
+import { buildTree, analyzeTree, depsPopulator, findUnused } from './utils';
+
+/**
+ * A function that maps an application, and returns an import web
+ * of strengths for each file within. Can be used to see which files
+ * have gone unused in a project, or are overweight, underutilised,
+ * or good candidates for module creation or deletion.
+ *
+ * TODO: the export weight isn't yet assigned, nor are functions
+ *    declared in such a way that you can track their names.
+ *    both would be good features; in that order.
+ * ALSO TODO LOL: fs is sync again because of our jank fs-mock lib /:
+ *
+ * @async
+ * @function arum
+ * @param {string} [rootPath] directory to analyse
+ * @returns {object} state tree with import statistics.
+ *    Should tree in the same way as the filesystem,
+ *    but import weights will be according to use regardless
+ *    of where the file is in physical space
+ */
+export default async rootPath => {
+  const fileTree = await buildTree(rootPath);
+
+  fileTree['package.json'] = depsPopulator(rootPath);
+
+  const analyzedTree = analyzeTree(fileTree, [(rootPath = '.')]);
+
+  const emptyFiles = findUnused(analyzedTree).filter(
+    e => !e.match('package.json')
+  );
+
+  if (emptyFiles.length > 0) {
+    console.log('Unused files found:\n', JSON.stringify(emptyFiles, null, 2));
+  }
+
+  return emptyFiles;
+};
--- test/index_test.js
+++ test/index_test.js
@@ -1,5 +1,29 @@
 import test from 'ava';
+import mock from 'mock-fs';
+import arum from '../src';
 
-test("doesn't do anything", t => {
-  t.truthy(true);
+test.afterEach.always(() => mock.restore());
+
+test.serial('does simple import weight analysis', async t => {
+  mock({
+    'package.json': `{
+      "dependencies": {
+        "markov-json": "a-version",
+        "ava": "totes-real-version-#"
+      }
+    }`,
+    '.gitignore': '#this must be here to keep mock-fs from dying',
+    'index.js': "import stuff from './src/doSomething';",
+    src: {
+      'doSomething.js': `
+        import MK from 'markov-json';
+        let basically_nothing = "doing";
+      `,
+      'unused.js': 'const fleeb = require("ava");'
+    }
+  });
+
+  const analysis = await arum();
+
+  t.deepEqual(analysis, ['./index.js', './src/unused.js']);
 });
