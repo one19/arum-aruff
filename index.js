1337f3d59b6d45e9c1d3874139cf75e95584f8e8
Drew Showalter <maelstroswim@gmail.com>
2018-08-11 22:11:03 +1000

    :wrench: properly build and execute bin

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-export * from './src';
+#!/usr/bin/env node
+import analyze from './src/index';
+
+const rootPath = process.argv[2];
+
+analyze(rootPath);
---
+++ shebang_it.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+/* eslint-disable */
+const fs = require('fs');
+
+const fileWords = fs.readFileSync('dist/index.js', { endoding: 'utf8' });
+
+try {
+  fs.unlinkSync('bin/index.js');
+} catch (e) { console.log('Unable to clear ./bin/index.js, continuing'); }
+
+try {
+  fs.mkdirSync('bin');
+} catch (_) {}
+
+try {
+  fs.writeFileSync('bin/index.js', `#!/usr/bin/env node\n${fileWords}`, { mode: '0755' });
+} catch (_) { throw new Error(`Unable to copy stuff into ./bin/index.js: ${e}`); }
